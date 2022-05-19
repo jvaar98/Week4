9f62f5fb895fe8e47148601938a19a2c1f7abb6b
Jaagup Vaar <jaagupvaar@MacBook-Air.lan>
2022-05-19 21:09:42 +0300

    add new projects

---
+++ DiceGame/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat - arvuti (cpu) ja kasutaja (user);
+            //mõlemad mängijad viskavad täringuid;
+            //programm kontrollib, kumb mängija viskas rohkem;
+            //mängija, kes viskab rohkem, ongi mängu võitja;
+            //*täringuid visatakse kolm korda;
+            //programm kuulutab võitja.
+
+            int i = 0;
+
+            while (i < 3)
+            {
+                Random rnd = new Random();
+
+                //arvuti vise
+                int cpuRandom = rnd.Next(1, 7);
+                //kasutaja vise
+                int userRandom = rnd.Next(1, 7);
+
+                int cpuScore = 0;
+                int userScore = 0;
+
+
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}");
+
+                if (cpuRandom < userRandom)
+                {
+                    i++;
+                    Console.WriteLine("Kasutaja on mängu võitnud. Palju õnne!");
+                    userScore = userScore + 1;
+
+                }
+                else if (cpuRandom > userRandom)
+                {
+                    i++;
+                    Console.WriteLine("Arvuti on mängu võitnud.");
+                    cpuScore = cpuScore + 1;
+
+                }
+                else
+                {
+                    i++;
+                    Console.WriteLine("Viik!");
+                }
+
+            }
+            
+            Console.WriteLine($"Arvuti lõpptulemus {cpuScore}. Kasutaja lõpptulemus {userScore}");
+        }
+    }
+}
---
+++ LoginValidation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus ja salasõna
+            //kui sisestatud kasutajatunnus on "admin" ja sisestatud salasõna on "admin1234"
+            //siis konsoolis kuvatakse "Tere tulemast!";
+            //muul konsoolis kovastakse "Vale kasutajatunnus või salasõna. Proovi uuesti."
+            //kasutajal on kolm katset
+
+            int i = 0;
+
+            while (i < 3) //while- loop >> i = 1; 1 < 3 --> true; 2 < 3 --> true; 3 < 3 --> false
+            {
+                Console.WriteLine("Sisesta kasutajatunnus:");
+                string userName = Console.ReadLine();
+                Console.WriteLine("Sisesta salasõna:");
+                string userPassword = Console.ReadLine();
+
+                if (userName == "admin" && userPassword == "admin1234")
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }
+                else
+                {
+                    //(i = i + 1) == i++
+                    i++;
+                    Console.WriteLine($"Vale kasutajatunnus või salasõna.{3 - i} katset on jäänud.");
+                }
+            }
+            Console.WriteLine("Head päeva!");
+
+        }
+    }
+}
